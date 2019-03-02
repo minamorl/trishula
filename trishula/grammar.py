f193673aeef957023c952d8bac75177dfe0e6547
minamorl <minamorl@minamorl.com>
2019-03-02 22:07:06 +0900

    Implement core features

--- trishula/grammar.py
+++ trishula/grammar.py
@@ -10,7 +10,22 @@ class Node:
         self.status = status
         self.index = index
 
-class Sequence:
+class OperatorMixin:
+    def __rshift__(self, other):
+        return Sequence(self, other)
+    def __or__(self, other):
+        return OrderedChoise(self, other)
+    def __invert__(self):
+        return ZeroOrMore(self)
+    def __invert__(self):
+        return ZeroOrMore(self)
+    def __pos__(self):
+        return OneOrMore(self)
+    def __neg__(self):
+        return Optional(self)
+
+
+class Sequence(OperatorMixin):
     def __init__(self, a, b):
         self.a = a
         self.b = b
@@ -21,22 +36,79 @@ class Sequence:
             return Node(Status.SUCCEED, resultB.index)
         return Node(Status.FAILED, i)
 
-class Value:
+
+class OrderedChoise(OperatorMixin):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def parse(self, target, i):
+        resultA = self.a.parse(target, i)
+        if resultA.status is Status.SUCCEED:
+            return Node(Status.SUCCEED, resuluA.index)
+        resultB = self.b.parse(target, resultA.index)
+        if resultB.status is Status.SUCCEED:
+            return Node(Status.SUCCEED, resultB.index)
+        return Node(Status.FAILED, i)
+
+
+class OneOrMore(OperatorMixin):
+    def __init__(self, a):
+        self.a = a
+    def parse(self, target, i):
+        return (self.a >> ZeroOrMore(self.a)).parse(target, i)
+
+
+class ZeroOrMore(OperatorMixin):
+    def __init__(self, a):
+        self.a = a
+    def parse(self, target, i):
+        result = self.a.parse(target, i)
+        if result.status is False or result.index == i:
+            return Node(Status.SUCCEED, result.index)
+        return self.parse(target, result.index)
+
+
+class Optional(OperatorMixin):
+    def __init__(self, a):
+        self.a = a
+    def parse(self, target, i):
+        result = self.a.parse(target, i)
+        return Node(Status.SUCCEED, result.index)
+
+
+class Value(OperatorMixin):
     def __init__(self, val):
         self.val = val
     def parse(self, target, i):
         if target[i:i + len(self.val)] == self.val:
             return Node(Status.SUCCEED, i + len(self.val))
         return Node(Status.FAILED, i)
-    def __rshift__(self, other):
-        return Sequence(self, other)
+
+
+class And(OperatorMixin):
+    def __init__(self, a):
+        self.a = a
+    def parse(self, target, i):
+        result = self.a.parse(target, i)
+        return Node(result.status, i)
+
+
+class Not(OperatorMixin):
+    def __init__(self, a):
+        self.a = a
+    def parse(self, target, i):
+        result = self.a.parse(target, i)
+        if result.status is Status.SUCCEED:
+            return Node(Status.FAILED, i)
+        return Node(Status.SUCCEED, i)
 
 
 class Parser:
     def parse(self, grammar, string):
-        return grammar.parse(string, 0)
+        result = grammar.parse(string, 0)
+        return Node(Status.SUCCEED if result.index == len(string) else Status.FAILED, result.index)
 
 
-grammar = Value("aaa") >> Value("bbb")
+grammar = Value("aaa") >> (Value("bbb") | Value("ccc")) >> +Value("eee") >> -Value("f") >> Value("g") >> Not(Value("hhh"))
 
-print(vars(Parser().parse(grammar, "aaabbb")))
+print(vars(Parser().parse(grammar, "aaaccceeeeeefghhh")))

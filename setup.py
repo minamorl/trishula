2a4d6f36c6925acf95008aea10297eca4198edfe
minamorl <minamorl@minamorl.com>
2019-03-03 22:54:55 +0900

    Add __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="minamorl",
+    author_email="minamorl@minamorl.com",
+    name="trishula",
+    url="https://github.com/minamorl/trishula",
+    version="0.0.1",
+    packages=find_packages(),
+)
---
+++ trishula/__init__.py
@@ -0,0 +1,1 @@
+from trishula.grammar import Regexp, And, Map, Node, Not, OneOrMore, Optional, OrderedChoice, Parser,Value, ZeroOrMore, Status, Sequence

99a2dddb561bbcb7e7ac38c7dc34592428d66529
minamorl <minamorl@minamorl.com>
2019-03-04 05:12:45 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     author_email="minamorl@minamorl.com",
     name="trishula",
     url="https://github.com/minamorl/trishula",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
 )

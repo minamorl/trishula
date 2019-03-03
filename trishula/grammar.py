f33f7c8cd53c1fcf66dda00bde02bb628b5d16c4
minamorl <minamorl@minamorl.com>
2019-03-03 19:15:01 +0900

    Use formatter

--- trishula/grammar.py
+++ trishula/grammar.py
@@ -1,28 +1,35 @@
 from enum import Enum, auto
 import re
 
+
 class Status(Enum):
     SUCCEED = auto()
     FAILED = auto()
 
 
 class Node:
-    def __init__(self, status, index, value = None):
+    def __init__(self, status, index, value=None):
         self.status = status
         self.index = index
         self.value = value
 
+
 class OperatorMixin:
     def __rshift__(self, other):
         return Sequence(self, other)
+
     def __or__(self, other):
         return OrderedChoise(self, other)
+
     def __invert__(self):
         return ZeroOrMore(self)
+
     def __invert__(self):
         return ZeroOrMore(self)
+
     def __pos__(self):
         return OneOrMore(self)
+
     def __neg__(self):
         return Optional(self)
 
@@ -31,6 +38,7 @@ class Sequence(OperatorMixin):
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
     def parse(self, target, i):
         resultA = self.a.parse(target, i)
         if resultA.status is Status.SUCCEED:
@@ -43,6 +51,7 @@ class OrderedChoise(OperatorMixin):
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
     def parse(self, target, i):
         resultA = self.a.parse(target, i)
         if resultA.status is Status.SUCCEED:
@@ -56,6 +65,7 @@ class OrderedChoise(OperatorMixin):
 class OneOrMore(OperatorMixin):
     def __init__(self, a):
         self.a = a
+
     def parse(self, target, i):
         result = (self.a >> ZeroOrMore(self.a)).parse(target, i)
         result.value = [result.value[0], *result.value[1]]
@@ -65,7 +75,8 @@ class OneOrMore(OperatorMixin):
 class ZeroOrMore(OperatorMixin):
     def __init__(self, a):
         self.a = a
-    def parse(self, target, i, values = []):
+
+    def parse(self, target, i, values=[]):
         result = self.a.parse(target, i)
         if result.status is False or result.index == i:
             return Node(Status.SUCCEED, result.index, values)
@@ -76,6 +87,7 @@ class ZeroOrMore(OperatorMixin):
 class Optional(OperatorMixin):
     def __init__(self, a):
         self.a = a
+
     def parse(self, target, i):
         result = self.a.parse(target, i)
         return Node(Status.SUCCEED, result.index, result.value)
@@ -84,8 +96,9 @@ class Optional(OperatorMixin):
 class Value(OperatorMixin):
     def __init__(self, val):
         self.val = val
+
     def parse(self, target, i):
-        if target[i:i + len(self.val)] == self.val:
+        if target[i : i + len(self.val)] == self.val:
             return Node(Status.SUCCEED, i + len(self.val), self.val)
         return Node(Status.FAILED, i)
 
@@ -93,6 +106,7 @@ class Value(OperatorMixin):
 class And(OperatorMixin):
     def __init__(self, a):
         self.a = a
+
     def parse(self, target, i):
         result = self.a.parse(target, i)
         return Node(result.status, i, result.value)
@@ -101,6 +115,7 @@ class And(OperatorMixin):
 class Not(OperatorMixin):
     def __init__(self, a):
         self.a = a
+
     def parse(self, target, i):
         result = self.a.parse(target, i)
         if result.status is Status.SUCCEED:
@@ -113,9 +128,11 @@ class Regexp(OperatorMixin):
         if isinstance(regexp, re.Pattern):
             regexp = regexp.pattern
         self.regexp = regexp
+
     def _anchored(self, regexp):
         regexp = f"^{self.regexp}"
         return regexp
+
     def parse(self, target, i):
         regexp = self._anchored(self.regexp)
         matched = re.match(regexp, target[i:])
@@ -127,11 +144,26 @@ class Regexp(OperatorMixin):
 class Parser:
     def parse(self, grammar, string):
         result = grammar.parse(string, 0)
-        return Node(Status.SUCCEED if result.index == len(string) else Status.FAILED, result.index, result.value)
+        return Node(
+            Status.SUCCEED if result.index == len(string) else Status.FAILED,
+            result.index,
+            result.value,
+        )
 
 
-grammar = Value("aaa") >> (Value("bbb") | Value("ccc")) >> +Value("eee") >> -Value("f") >> Value("g") >> Regexp(r"a+")# >> Not(Value("hhh"))
+grammar = (
+    Value("aaa")
+    >> (Value("bbb") | Value("ccc"))
+    >> +Value("eee")
+    >> -Value("f")
+    >> Value("g")
+    >> Regexp(r"a+")
+)  # >> Not(Value("hhh"))
 # This works
 print(vars(Parser().parse(grammar, "aaaccceeeeeeeeeeeefgaaa")))
 # ==>
-# {'status': <Status.SUCCEED: 1>, 'index': 23, 'value': [[[[['aaa', 'ccc'], ['eee', 'eee', 'eee', 'eee']], 'f'], 'g'], 'aaa']}
+# {
+#    'status': <Status.SUCCEED: 1>,
+#    'index': 23,
+#    'value': [[[[['aaa', 'ccc'], ['eee', 'eee', 'eee', 'eee']], 'f'], 'g'], 'aaa']}
+# }

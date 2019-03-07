2bc8c7c0e8b398c69aa3384a71d93955c92495ed
minamorl <minamorl@minamorl.com>
2019-03-08 01:29:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     author="minamorl",
     author_email="minamorl@minamorl.com",
     name="trishula",
     url="https://github.com/minamorl/trishula",
+    description="The modern PEG parser combinator for python",
+    long_description=long_description,
     version="0.0.5",
     packages=find_packages(),
 )

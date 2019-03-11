686c65b7ec684677596954278b8524f5b7b48a4a
minamorl <minamorl@minamorl.com>
2019-03-12 00:12:49 +0900

    wip

--- example/calculator.py
+++ example/calculator.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import operator
+import pprint
+import unittest as U
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -12,6 +14,9 @@ def realnum(d):
         return float(d["integer"] + "." + d["fractional"])
     return float(d["integer"])
 
+def setParenthesis(d):
+    d["wrappedByParenthesis"] = True
+    return d
 
 term = (
     (
@@ -29,22 +34,57 @@ term = (
 
 space = T.Value(" ")
 
-parenthesis = T.Namespace(
+parenthesis = (T.Namespace(
     T.Value("(") >> (T.Ref(lambda: grammar) @ "value") >> T.Value(")")
-) >= (lambda d: d["value"])
+) >= (lambda d: d["value"])) >= setParenthesis
 
+
+def replaceOperator(d, fn):
+    d["operator"] = fn
+    return d
+
+def left_rotation(d):
+    if not isinstance(d, dict):
+        return d
+    if isinstance(d["right"], float):
+        return d
+    if d["operator"] is operator.add or d["operator"] is operator.sub:
+        return right_rotation(d)
+
+    A = d["left"]
+    B = d["right"]["left"]
+    C = d["right"]["right"]
+    P = d["operator"]
+    Q = d["right"]["operator"]
+
+    result = {"left": {"left": A, "operator": P, "right": B}, "operator": Q, "right": C}
+    return left_rotation(result)
+
+def right_rotation(d):
+    if isinstance(d, float):
+        return d
+    if isinstance(d["left"], float):
+        return d
+    A = d["left"]["left"]
+    B = d["left"]["right"]
+    C = d["right"]
+    P = d["left"]["operator"]
+    Q = d["operator"]
+
+    result = {"left": A, "operator": P, "right": {"left": B, "operator": Q, "right": C}}
+    return right_rotation(result)
 
 def op(string, fn):
-    return T.Namespace(
-        ((term | parenthesis) @ "_1")
+    return (T.Namespace(
+        ((term | parenthesis) @ "left")
         >> -space
         >> (T.Value(string) @ "operator")
-        >> (-(-space >> (T.Ref(lambda: grammar) @ "grammar")))
-    ) >= (lambda d: fn(d["_1"], d["grammar"]))
+        >> (-(-space >> (T.Ref(lambda: grammar) @ "right")))
+    ) >= (lambda d: replaceOperator(d, fn)))
 
 
 grammar = (
-    op("*", operator.mul)
+   op("*", operator.mul)
     | op("/", operator.truediv)
     | op("+", operator.add)
     | op("-", operator.sub)
@@ -59,14 +99,40 @@ items = (
     "1 / 2",
     "1 + 2 * -3 / 4",
     "1 + 2 / 2",
+    "1 / 2 / 2",
+    "1 * 2 / 3",
     "(1 + 2) * 3",
     "(1 - (2 - (3 - 4)))",
     "1.4 * 3",
+    "(1 + (2 / 3))",
+    "(2 * 3) / 4",
+    "1 + 2 / 3",
+    "((0 - (1 * 2) / 3) / 4 / 5 ) * 6"
 )
 
+
+
+
+def calc(d):
+    if not isinstance(d, dict):
+        return d
+    left = calc(d["left"])
+    right = calc(d["right"])
+    return d["operator"](left, right)
+
+
+
+
 for x in items:
-    result = T.Parser().parse(grammar, x)
-    print(vars(result))
+    result = T.Parser().parse(grammar, x).value
+    if float(eval(x)) != calc(left_rotation(result)):
+        pprint.pprint(((result)))
+        pprint.pprint((left_rotation(result)))
+        print(x)
+        print(float(eval(x)))
+        print(calc(left_rotation(result)))
+
+        print()
 
 # Result is:
 #

fa228fa2d9fa40caef6e9ddbdc1e63c445e08c94
minamorl <minamorl@minamorl.com>
2019-03-12 01:16:48 +0900

    wip

--- example/calculator.py
+++ example/calculator.py
@@ -14,10 +14,6 @@ def realnum(d):
         return float(d["integer"] + "." + d["fractional"])
     return float(d["integer"])
 
-def setParenthesis(d):
-    d["wrappedByParenthesis"] = True
-    return d
-
 term = (
     (
         (-(T.Value("-")) >= (lambda x: -1.0 if x is not None else 1.0))
@@ -34,53 +30,22 @@ term = (
 
 space = T.Value(" ")
 
-parenthesis = (T.Namespace(
+parenthesis = T.Namespace(
     T.Value("(") >> (T.Ref(lambda: grammar) @ "value") >> T.Value(")")
-) >= (lambda d: d["value"])) >= setParenthesis
+) >= (lambda d: d["value"])
 
 
 def replaceOperator(d, fn):
     d["operator"] = fn
     return d
 
-def left_rotation(d):
-    if not isinstance(d, dict):
-        return d
-    if isinstance(d["right"], float):
-        return d
-    if d["operator"] is operator.add or d["operator"] is operator.sub:
-        return right_rotation(d)
-
-    A = d["left"]
-    B = d["right"]["left"]
-    C = d["right"]["right"]
-    P = d["operator"]
-    Q = d["right"]["operator"]
-
-    result = {"left": {"left": A, "operator": P, "right": B}, "operator": Q, "right": C}
-    return left_rotation(result)
-
-def right_rotation(d):
-    if isinstance(d, float):
-        return d
-    if isinstance(d["left"], float):
-        return d
-    A = d["left"]["left"]
-    B = d["left"]["right"]
-    C = d["right"]
-    P = d["left"]["operator"]
-    Q = d["operator"]
-
-    result = {"left": A, "operator": P, "right": {"left": B, "operator": Q, "right": C}}
-    return right_rotation(result)
-
 def op(string, fn):
-    return (T.Namespace(
+    return T.Namespace(
         ((term | parenthesis) @ "left")
         >> -space
         >> (T.Value(string) @ "operator")
         >> (-(-space >> (T.Ref(lambda: grammar) @ "right")))
-    ) >= (lambda d: replaceOperator(d, fn)))
+    ) >= (lambda d: replaceOperator(d, fn))
 
 
 grammar = (
@@ -100,6 +65,7 @@ items = (
     "1 + 2 * -3 / 4",
     "1 + 2 / 2",
     "1 / 2 / 2",
+    "1 / 2 / 2 / 2 / 2",
     "1 * 2 / 3",
     "(1 + 2) * 3",
     "(1 - (2 - (3 - 4)))",
@@ -107,10 +73,42 @@ items = (
     "(1 + (2 / 3))",
     "(2 * 3) / 4",
     "1 + 2 / 3",
-    "((0 - (1 * 2) / 3) / 4 / 5 ) * 6"
+    "((1)) * 2",
+    "(-1 * (0 - (1 * 2) / 3) / 4 / 5) * 6 / 7"
 )
 
 
+
+def left_rotation(d):
+    if not isinstance(d, dict):
+        return d
+    if isinstance(d["right"], float):
+        return d
+    if d["operator"] is operator.add or d["operator"] is operator.sub:
+        return right_rotation(d)
+
+    A = d["left"]
+    B = d["right"]["left"]
+    C = d["right"]["right"]
+    P = d["operator"]
+    Q = d["right"]["operator"]
+
+    result = {"left": {"left": A, "operator": P, "right": B}, "operator": Q, "right": C}
+    return left_rotation(result)
+
+def right_rotation(d):
+    if not isinstance(d, dict):
+        return d
+    if isinstance(d["left"], float):
+        return d
+    A = d["left"]["left"]
+    B = d["left"]["right"]
+    C = d["right"]
+    P = d["left"]["operator"]
+    Q = d["operator"]
+
+    result = {"left": A, "operator": P, "right": {"left": B, "operator": Q, "right": C}}
+    return right_rotation(result)
 
 
 def calc(d):
@@ -133,15 +131,5 @@ for x in items:
         print(calc(left_rotation(result)))
 
         print()
-
-# Result is:
-#
-#  {'status': <Status.SUCCEED: 1>, 'index': 1, 'value': 0.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 4, 'value': -123.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 3, 'value': 123.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 5, 'value': 0.5, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 14, 'value': -0.5, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 9, 'value': 2.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 11, 'value': 9.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 19, 'value': -2.0, 'namespace': {}}
-#  {'status': <Status.SUCCEED: 1>, 'index': 7, 'value': 4.199999999999999, 'namespace': {}}
+    else:
+        print(f"{x} is {calc(left_rotation(result))}")
